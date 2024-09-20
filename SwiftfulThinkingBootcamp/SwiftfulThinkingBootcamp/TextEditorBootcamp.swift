6d08fe000f1ba32ec5493dcb3107e43443815f6f
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-09-21 00:11:48 +0200

    TextEditorBootcamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TextEditorBootcamp.swift
@@ -0,0 +1,47 @@
+//
+//  TextEditorBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Miguel Mañas Alvarez on 21/9/24.
+//
+
+import SwiftUI
+
+struct TextEditorBootcamp: View {
+    
+    @State var textEditorText: String = "This is the starting text"
+    @State var savedText: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $textEditorText)
+                    .frame(height: 250)
+//                    .foregroundColor(.red)
+                    .colorMultiply(Color(uiColor: .systemGray4))
+                Button {
+                    savedText = textEditorText
+                } label: {
+                    Text("Save".uppercased())
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+                Text(savedText)
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("TextEditor Bootcamp!")
+        }
+    }
+}
+
+struct TextEditorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditorBootcamp()
+    }
+}
