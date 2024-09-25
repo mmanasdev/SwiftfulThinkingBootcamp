73be2620604dcfb5e328575f071342db4f29ca17
miguelmanasdev <miguel.manas.alvarez.dev@gmail.com>
2024-09-25 11:44:48 +0200

    TabViewBootcamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/TabViewBootcamp.swift
@@ -0,0 +1,89 @@
+//
+//  TabViewBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Miguel Mañas Alvarez on 25/9/24.
+//
+
+import SwiftUI
+
+struct TabViewBootcamp: View {
+    
+    @State var selectedTab: Int = 2
+    let icons: [String] = ["heart.fill", "globe", "house.fill", "person.fill"]
+    
+    var body: some View {
+//        TabView(selection: $selectedTab) {
+//            HomeView(selectedTab: $selectedTab)
+//                .tabItem {
+//                    Image(systemName: "house.fill")
+//                    Text("Home")
+//                }
+//                .tag(0)
+//
+//            Text("BROWSE TAB")
+//                .tabItem {
+//                    Image(systemName: "globe")
+//                    Text("Browse")
+//                }
+//                .tag(1)
+//
+//            Text("PROFILE TAB")
+//                .tabItem {
+//                    Image(systemName: "person.fill")
+//                    Text("Profile")
+//                }
+//                .tag(2)
+//        }
+//        .accentColor(.red)
+        
+        TabView {
+            ForEach(icons, id: \.self) { icon in
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(30)
+                
+            }
+        }
+        .background(
+            RadialGradient(gradient: Gradient(colors: [.red, .blue]), center: .center, startRadius: 5, endRadius: 300)
+        )
+        .frame(height: 300)
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+struct TabViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TabViewBootcamp()
+    }
+}
+
+struct HomeView: View {
+    
+    @Binding var selectedTab: Int
+    
+    var body: some View {
+        ZStack {
+            Color.red
+                .edgesIgnoringSafeArea(.top)
+            VStack {
+                Text("HOME TAB")
+                    .font(.largeTitle)
+                .foregroundColor(.white)
+                
+                Button {
+                    selectedTab = 2
+                } label: {
+                    Text("Go to Profile")
+                        .font(.headline)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
